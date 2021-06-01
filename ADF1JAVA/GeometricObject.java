006d5f7cc83dd5f035f5084d0b5d581e2d47fd74
Hoang van Anh (APTECH HN) <anhhvth2010043@fpt.edu.vn>
2021-06-01 10:46:34 +0700

    ADF1JAVA

--- ADF1JAVA/GeometricObject.java
+++ ADF1JAVA/GeometricObject.java
@@ -4,30 +4,38 @@ import java.util.Date;
 public abstract class GeometricObject {
     private String color = "white";
     private boolean filled;
-    private java.util.Date dateCreated;
-    /** Construct a default geometric object */
+    private Date dateCreated;
     protected GeometricObject() {
-    dateCreated = new java.util.Date();
+    dateCreated = new Date();
     }
-    /** Construct a geometric object with the specified color
-    * and filled value */
+
     protected GeometricObject(String color, boolean filled) {
     this.color = color;
     this.filled = filled;
     }
-    /** Return color */
-    public String getColor() { return color; }
-    public void setColor(String color) { this.color = color; }
-    public boolean isFilled() { return filled; }
-    public void setFilled(boolean filled) { this.filled = filled; }
-    public java.util.Date getDateCreated() { return dateCreated; }
+
+    public String getColor() {
+         return color;
+         }
+    public void setColor(String color) { 
+        this.color = color;
+     }
+    public boolean isFilled() {
+         return filled; 
+        }
+    public void setFilled(boolean filled) {
+         this.filled = filled; 
+        }
+    public Date getDateCreated() {
+         return dateCreated; 
+        }
     public String toString() {
    return "created on " + dateCreated + "\ncolor: " + color +
     " and filled: " + filled;
     }
-    /** Abstract method getArea */
+
     public abstract double getArea();
-    /** Abstract method getPerimeter */
+    
     public abstract double getPerimeter();
    
     
--- ADF1JAVA/TestGeometricObject.java
+++ ADF1JAVA/TestGeometricObject.java
@@ -3,7 +3,7 @@
 public class TestGeometricObject {
     public static void main(String[] args)
     {
-    // create two geometric objects
+   
     GeometricObject geoObject1 = new Circle(5);
     GeometricObject geoObject2 = new Rectangle(5, 3);
     System.out.println("The two objects have the same area? " +
