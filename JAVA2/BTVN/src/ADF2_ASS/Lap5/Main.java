a2d78d446a4f3b3092ae5a4730fed052ce231a92
Hoang van Anh (APTECH HN) <anhhvth2010043@fpt.edu.vn>
2021-06-10 09:22:16 +0700

    java_2

--- JAVA2/BTVN/src/ADF2_ASS/Lap5/Main.java
+++ JAVA2/BTVN/src/ADF2_ASS/Lap5/Main.java
@@ -1,15 +1,10 @@
 package ADF2_ASS.Lap5;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Main {
     static List<Student> studentList = new ArrayList<>();
@@ -27,34 +22,50 @@ public class Main {
                     inputSudent();
                     break;
                 } case  2 : {
-                    for(Student student : studentList){
-                        student.display();
-                    }
+                    editStudentById();
                     break;
                 }
                 case  3: {
-                    saveFile();
+                    deleteStudentById();
                     break;
                 }
                 case  4 : {
+                    sortSudentByName();
+                    break;
+                }
+                case  5 : {
+
+                    displayStudent();
+
+                    break;
+                }
+                case  6 : {
+                    saveFile();
+                    break;
+                }
+                case  7 : {
                     readFile();
                     break;
                 }
+
                 default: {
                     System.out.println("Nhap sai");
                     break;
                 }
             }
-        }while (choose != 5);
+        }while (choose != 8);
 
     }
 
     static  void showMenu(){
         System.out.println("1 . Them sinh vien");
-        System.out.println("2 . thong tin sin vien");
-        System.out.println("3 . Lua thong tin vao file student.txt");
-        System.out.println("4. Doc not dung sinh vien tu student.txt");
-        System.out.println("5 . exit ");
+        System.out.println("2 . Sua Sinh Vien ");
+        System.out.println("3 . Xoa Sinh Vien");
+        System.out.println("4 . Tim Kiem sinh vien qua name");
+        System.out.println("5 . thong tin sin vien");
+        System.out.println("6 . Lua thong tin vao file student.txt");
+        System.out.println("7. Doc not dung sinh vien tu student.txt");
+        System.out.println("8. exit ");
     }
 
     private static  void inputSudent(){
@@ -68,6 +79,45 @@ public class Main {
 
     }
 
+    private  static  void  editStudentById(){
+        System.out.println("Nhap id sinh vien can sua :");
+        int id = input.nextInt();
+        for(Student student : studentList){
+            if(student.getId() == id){
+                student.input();
+                break;
+            }
+        }
+    }
+
+    private  static  void deleteStudentById(){
+        System.out.println("Nhap id sinh vien can xoa : ");
+        int id = input.nextInt();
+
+        for(Student student : studentList){
+            if(student.getId() == id){
+                studentList.remove(student);
+                break;
+            }
+        }
+
+    }
+
+    public  static  void sortSudentByName(){
+        int id = input.nextInt();
+       for(Student student : studentList){
+           if(student.getId() == id ){
+               student.display();
+               break;
+           }
+       }
+    }
+
+    public  static  void displayStudent(){
+        for(Student student : studentList){
+            student.display();
+        }
+    }
     private  static void saveFile(){
         System.out.println("Lua File : ");
         FileOutputStream fos = null;
